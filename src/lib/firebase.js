d296773a178b7a64e2977983562b108fb8bfebb1
Cress1004 <nuhoangchenhac@gmail.com>
2021-05-19 20:48:45 +0700

    Use Firebase

---
+++ src/lib/firebase.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDhV49ne7l6kQPdGb_JTBu_E76z0FGIfns",
+    authDomain: "fb-itss.firebaseapp.com",
+    projectId: "fb-itss",
+    storageBucket: "fb-itss.appspot.com",
+    messagingSenderId: "688257836805",
+    appId: "1:688257836805:web:1a39048b9fba25eab6b96c"
+}
+
+firebase.initializeApp(firebaseConfig);
